7df447bed76a42592cf2e33e96dd7e15641d1446
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-19 13:27:05 -0400

    saving

--- src/main/java/user_management/UserCollection.java
+++ src/main/java/user_management/UserCollection.java
@@ -1,22 +1,33 @@
 package user_management;
 
+import user_management.security.UserAuthenticationFailedException;
+import user_management.validation.EmailNotAvailableException;
+import user_management.validation.InvalidEmailException;
+import user_management.validation.PasswordTooSimpleException;
+
 import java.util.ArrayList;
 
 public class UserCollection extends ArrayList<User> {
 
     public User findById(int id) {
+        for(User u : this){
+            if(u.getId()==id) return u;
+        }
         return null;
     }
 
     public User findByEmail(String email) {
+        for(User u : this){
+            if(u.getEmail().equals(email)) return u;
+        }
         return null;
     }
 
-    public User attemptLogin(String email, String password) {
+    public User attemptLogin(String email, String password) throws UserAuthenticationFailedException {
         return null;
     }
 
-    public int createUser(String name, String email, String password) {
+    public int createUser(String name, String email, String password) throws PasswordTooSimpleException,EmailNotAvailableException, InvalidEmailException {
         return 0;
     }
 }
