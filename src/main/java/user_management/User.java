80c8159817433214a0610dd2141903d54c813965
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-19 13:09:55 -0400

    making progress in part 2

--- src/main/java/user_management/User.java
+++ src/main/java/user_management/User.java
@@ -3,14 +3,77 @@ package user_management;
 import user_management.security.Password;
 
 public class User {
+    private String email;
+    private int id;
+    private String name;
+    private Password password;
+
+    public User( int id, String name,String email, Password password){
+        this.email=email;
+        this.id=id;
+        this.name=name;
+        this.password=password;
+    }
+    public User( int id, String name,String email, String password){
+        this.email=email;
+        this.id=id;
+        this.name=name;
+        this.password= new Password(password);
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+
+    public void setPassword(String password) {
+        this.password = new Password(password);
+    }
+
 
     @Override
     public String toString() {
-        return null;
+
+        return this.name + " - " + this.email;
     }
 
     @Override
     public boolean equals(Object obj) {
-        return false;
+        if(obj==this) return true;
+        if(!(obj instanceof User)) return false;
+
+        User other = (User) obj;
+
+        return this.email.equals(other.getEmail()) && this.id == other.getId()
+                && this.name.equals(other.getName());
+
     }
 }
--- src/main/java/user_management/UserCollectionInitializer.java
+++ src/main/java/user_management/UserCollectionInitializer.java
@@ -1,7 +1,20 @@
 package user_management;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class UserCollectionInitializer {
-    public static UserCollection generate() {
-        return null;
+    public static UserCollection generate() throws FileNotFoundException {
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader("/Users/ericbarnaba/Dev/test3/src/main/resources/users.json"));
+        UserCollection users = new UserCollection();
+        User[] array = gson.fromJson(reader,User[].class);
+        users.addAll(Arrays.asList(array));
+
+        return users;
     }
 }
