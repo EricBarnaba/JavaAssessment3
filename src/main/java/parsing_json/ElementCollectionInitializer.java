76f2bbd8dc47bdbbde592f9957856f82b68b2631
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-19 10:28:07 -0400

    element collection initializer working

--- src/main/java/parsing_json/ElementCollectionInitializer.java
+++ src/main/java/parsing_json/ElementCollectionInitializer.java
@@ -1,7 +1,18 @@
 package parsing_json;
 
-public class ElementCollectionInitializer {
-    public static ElementCollection generate() {
-        return null;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ElementCollectionInitializer  {
+
+    public static ElementCollection generate() throws FileNotFoundException {
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader("/Users/ericbarnaba/Dev/test3/src/main/resources/periodic_table.json"));
+        ElementCollection elements = gson.fromJson(reader,ElementCollection.class);
+
+        return elements;
     }
 }
